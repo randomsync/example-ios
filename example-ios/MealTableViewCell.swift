5387925d7eba6a2aabbaac3f5ff7f256d1425e94
Gaurav Gupta <gaurav@randomsync.net>
2016-10-31 22:46:16 -0700

    Initial implementation of table view

---
+++ example-ios/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  example-ios
+//
+//  Created by Gaurav Gupta on 10/31/16.
+//  Copyright © 2016 Gaurav Gupta. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
